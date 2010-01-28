a0be8837fcf6e8787d86472c6fcdac9b0876e8e7
Brandon Weiss <brandon@anti-pattern.com>
2010-01-27 20:40:20 -0600

    Added color parameter

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,19 +1,21 @@
 (function($) {
 
-	$.fn.placeholder = function() {
+	$.fn.placeholder = function(color) {
+		var color = color || '#a9a9a9';
+		
 		if (!$.browser.webkit) {
 			var $input_fields = $(this).filter('input[type=text]');
 			
 			$input_fields
 				.val($input_fields.attr('placeholder'))
-				.css('color', '#707070');			
+				.css('color', color);			
 				
 			$input_fields.focus(function() {
-				if ($(this).data("placeholder_original_value") == undefined) {
-					$(this).data("placeholder_original_value", $(this).val());
+				if ($(this).data("placeholder_text") == undefined) {
+					$(this).data("placeholder_text", $(this).val());
 				}
 			
-				if ($(this).val() == $(this).data("placeholder_original_value")) {
+				if ($(this).val() == $(this).data("placeholder_text")) {
 				 	$(this)
 						.val('')
 						.css('color', '');
@@ -23,11 +25,12 @@
 			$input_fields.blur(function() {
 				if ($(this).val() == '') {
 					$(this)
-						.val($(this).data("placeholder_original_value"))
-						.css('color', '#707070');			
+						.val($(this).data("placeholder_text"))
+						.css('color', color);			
 				}
 			});
 		}
 	};
 
 })(jQuery);
+
