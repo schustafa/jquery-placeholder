90945ac4130591b3abf8057a45585b0b582550d9
Brandon Weiss <brandon@anti-pattern.com>
2010-01-27 22:17:36 -0600

    Converted to soft tabs . . . I think

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,35 +1,34 @@
 (function($) {
 
-	$.fn.placeholder = function(color) {
-		if (!$.browser.webkit) {
-			var color = color || '#a9a9a9';
+  $.fn.placeholder = function(color) {
+    if (!$.browser.webkit) {
+      var color = color || '#a9a9a9';
 
-			var $input_fields = $(this).filter('input[type=text]');
+      var $input_fields = $(this).filter('input[type=text]');
 			
-			$input_fields
-				.val($input_fields.attr('placeholder'))
-				.css('color', color);			
+      $input_fields
+        .val($input_fields.attr('placeholder'))
+        .css('color', color);			
 				
-			$input_fields.focus(function() {
-				if ($(this).data("placeholder_text") == undefined) {
-					$(this).data("placeholder_text", $(this).val());
-				}
+      $input_fields.focus(function() {
+        if ($(this).data("placeholder_text") == undefined)
+          $(this).data("placeholder_text", $(this).val());
 			
-				if ($(this).val() == $(this).data("placeholder_text")) {
-				 	$(this)
-						.val('')
-						.css('color', '');
-				}
-			});
+        if ($(this).val() == $(this).data("placeholder_text")) {
+          $(this)
+            .val('')
+            .css('color', '');
+        }
+      });
 			
-			$input_fields.blur(function() {
-				if ($(this).val() == '') {
-					$(this)
-						.val($(this).data("placeholder_text"))
-						.css('color', color);			
-				}
-			});
-		}
-	};
+      $input_fields.blur(function() {
+        if ($(this).val() == '') {
+          $(this)
+            .val($(this).data("placeholder_text"))
+            .css('color', color);			
+        }
+      });
+    }
+  };
 
 })(jQuery);
